042368edce91a60914253604302aa38f73b36bae
Egor Santamaria <esantamaria@vunkers.com>
2024-04-26 11:23:17 +0200

    Navegar

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.addEventListener('load', () => {
 
     const principalUrl = 'https://fakestoreapi.com';
     let actualCategory = 'electronics';
+    const nav = document.querySelector('nav');
 
     const retrieveData =  async function(addInfo = '') {
         const response = await fetch(`${principalUrl}/${addInfo}`);
@@ -20,8 +21,6 @@ window.addEventListener('load', () => {
     }
 
     retrieveData('products/categories').then((categories) => {
-        const nav = document.querySelector('nav');
-
         categories.forEach((category, index) => {
             createElement(nav, 'a', category);
         
@@ -30,23 +29,38 @@ window.addEventListener('load', () => {
 
     });
 
-    retrieveData(`products/category/${actualCategory}`).then((products) => {
-        const productsParent = document.querySelector('.products');
-
-        products.forEach((product) => {
-            const tempProduct = document.createElement('div');
-
-            const img = document.createElement('img');
-            img.src = product.image;
-            tempProduct.appendChild(img);
-
-            createElement(tempProduct, 'h4', product.title);
-            createElement(tempProduct, 'p', product.description);
-            createElement(tempProduct, 'span', product.price);
-
-            productsParent.appendChild(tempProduct);
+    const retrieveProducts = (url) => {
+        retrieveData(`${url}`).then((products) => {
+            document.querySelector('.products').remove();
+            const productsParent = document.createElement('div');
+            productsParent.className = 'products';
+            document.querySelector('body').appendChild(productsParent);
+    
+            products.forEach((product) => {
+                const tempProduct = document.createElement('div');
+    
+                const img = document.createElement('img');
+                img.src = product.image;
+                tempProduct.appendChild(img);
+    
+                createElement(tempProduct, 'h4', product.title);
+                createElement(tempProduct, 'p', product.description);
+                createElement(tempProduct, 'span', product.price);
+    
+                productsParent.appendChild(tempProduct);
+            });
+    
         });
+    }
 
+    retrieveProducts(`products/category/${actualCategory}`);
+
+    nav.addEventListener('click', (elem) => {
+        const text = elem.target.textContent || '';
+
+        if(text.length > 0){
+            retrieveProducts(`products/category/${text}`);
+        }
     });
 
 }); 
