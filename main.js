3d231321405a49654a8fdd59de15c4b3cc92d618
Egor Santamaria <esantamaria@vunkers.com>
2024-04-26 11:12:40 +0200

    Mostrar productes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
 
     const principalUrl = 'https://fakestoreapi.com';
+    let actualCategory = 'electronics';
 
     const retrieveData =  async function(addInfo = '') {
         const response = await fetch(`${principalUrl}/${addInfo}`);
@@ -12,19 +13,38 @@ window.addEventListener('load', () => {
         const element = document.createElement(type);
 
         if(text.length > 0){
-            console.log('append');
             element.innerHTML = text;  
         }
-
+        
         parent.appendChild(element);
     }
 
     retrieveData('products/categories').then((categories) => {
         const nav = document.querySelector('nav');
 
-        categories.forEach((category) => {
+        categories.forEach((category, index) => {
             createElement(nav, 'a', category);
-            
+        
+            if(index === 0) actualCategory = category;
+        });
+
+    });
+
+    retrieveData(`products/category/${actualCategory}`).then((products) => {
+        const productsParent = document.querySelector('.products');
+
+        products.forEach((product) => {
+            const tempProduct = document.createElement('div');
+
+            const img = document.createElement('img');
+            img.src = product.image;
+            tempProduct.appendChild(img);
+
+            createElement(tempProduct, 'h4', product.title);
+            createElement(tempProduct, 'p', product.description);
+            createElement(tempProduct, 'span', product.price);
+
+            productsParent.appendChild(tempProduct);
         });
 
     });
